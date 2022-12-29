e78b2f0b681b83ad90ad7eeafc2f9ea3047e0d2c
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-12-29 17:34:48 +0200

    fixed query

--- src/fetch-api/search-movies.js
+++ src/fetch-api/search-movies.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '09a141b997a10edba2747d029eb90ca7';
 
 export async function getSearchMovies(query) {
   const response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie/?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+    `${BASE_URL}/search/movie/?api_key=${API_KEY}&&query=${query}&language=en-US&page=1&include_adult=false`
   );
 
   console.log(response.data);
